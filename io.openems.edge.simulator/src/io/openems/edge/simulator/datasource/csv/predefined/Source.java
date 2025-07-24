ef270d106a60d9d89e75c0c0816510bac469a3d1
Fabian Peters <fabian@mercadu.de>
2025-07-24 12:19:48 +0200

    Add PURE_LOAD.csv, update configuration & Grafana

--- io.openems.edge.simulator/src/io/openems/edge/simulator/datasource/csv/predefined/Source.java
+++ io.openems.edge.simulator/src/io/openems/edge/simulator/datasource/csv/predefined/Source.java
@@ -7,6 +7,7 @@ public enum Source {
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_NON_REGULATED_CONSUMPTION("h0-summer-weekday-non-regulated-consumption.csv"), //
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_PV_PRODUCTION2("h0-summer-weekday-pv-production2.csv"), //
 	MINI_GRID_LOAD("MINI_GRID_LOAD.csv"), //
+	PURE_LOAD("PURE_LOAD.csv"), //
 	PV_PROD_METER_ACTING("PV_PROD_METER_ACTING.csv");
 
 	public final String filename;
