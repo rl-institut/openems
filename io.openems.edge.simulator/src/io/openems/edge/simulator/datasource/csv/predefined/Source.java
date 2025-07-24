7161f0641a46711b95860b457f3e5c3f8319a13f
Fabian Peters <fabian@mercadu.de>
2025-07-24 09:52:02 +0200

    Add custom CSV profile files

--- io.openems.edge.simulator/src/io/openems/edge/simulator/datasource/csv/predefined/Source.java
+++ io.openems.edge.simulator/src/io/openems/edge/simulator/datasource/csv/predefined/Source.java
@@ -5,7 +5,9 @@ public enum Source {
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_STANDARD_LOAD_PROFILE("h0-summer-weekday-standard-load-profile.csv"), //
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_PV_PRODUCTION("h0-summer-weekday-pv-production.csv"), //
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_NON_REGULATED_CONSUMPTION("h0-summer-weekday-non-regulated-consumption.csv"), //
-	H0_HOUSEHOLD_SUMMER_WEEKDAY_PV_PRODUCTION2("h0-summer-weekday-pv-production2.csv");
+	H0_HOUSEHOLD_SUMMER_WEEKDAY_PV_PRODUCTION2("h0-summer-weekday-pv-production2.csv"), //
+	MINI_GRID_LOAD("MINI_GRID_LOAD.csv"), //
+	PV_PROD_METER_ACTING("PV_PROD_METER_ACTING.csv");
 
 	public final String filename;
 
